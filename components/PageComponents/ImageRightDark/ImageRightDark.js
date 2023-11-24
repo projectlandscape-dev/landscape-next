dfc07146e607e1033fac418845b00c1ffd085e65
loveheenvasu <trivediheena@gmail.com>
2023-11-24 09:53:58 +0530

    fix button import

--- components/PageComponents/ImageRightDark/ImageRightDark.js
+++ components/PageComponents/ImageRightDark/ImageRightDark.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { ButtonSecondaryLight } from "../buttons";
+import { ButtonSecondaryLight } from "../../buttons";
 import { Container, Flex, FlexMobileOpp, Section } from "../layoutComponents";
 import Image from "next/image";
 
