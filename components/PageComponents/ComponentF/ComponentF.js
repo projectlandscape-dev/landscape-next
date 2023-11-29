4fd3e1474d4b83359dd2469087369448bd199c6c
loveheenvasu <trivediheena@gmail.com>
2023-11-29 12:55:23 +0530

    add new section

---
+++ components/PageComponents/ComponentF/ComponentF.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Section, Container, GridThree } from "components/layoutComponents";
+import { ButtonPrimary } from "components/buttons";
+import Image from "components/Image";
+import { BsTextWrap } from "react-icons/bs";
+
+const Text = styled.div`
+  text-align: center;
+  max-width: 110ch;
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+// const StyledImg = styled(GatsbyImage)`
+//   height: 300px;
+//   width: 100%;
+// `
+
+const Item = styled.div`
+  text-align: center;
+
+  border: 3px solid var(--clr-tan);
+  border-radius: var(--br);
+
+  figure {
+    div {
+      img {
+        height: 300px;
+        object-fit: cover;
+      }
+    }
+  }
+`;
+
+const Inner = styled.div`
+  padding: 2em;
+  cursor: pointer;
+`;
+
+const AdditionalText = styled.div`
+  display: ${({ expanded }) => (expanded ? "block" : "none")};
+`;
+
+export default function ComponentF({ subheader, title, content }) {
+  const [isTextExpanded, setIsTextExpanded] = useState(false);
+
+  const handleInnerClick = () => {
+    setIsTextExpanded(!isTextExpanded);
+  };
+  let width = "100%";
+  let height = "300px";
+  return (
+    <Section>
+      <Container className="spacing">
+        <Text className="spacing">
+          <div className="">
+            <p className="subheader accent">{subheader}</p>
+            <h2 className="title">{title}</h2>
+          </div>
+        </Text>
+        <center>
+          <GridThree>
+            {content?.map((item) => {
+              return (
+                <Item className="spacing">
+                  <Image
+                    alt={item.image.altText || ""}
+                    srcSet={item.image.srcSet}
+                    src={item.image.src}
+                    width={width}
+                    height={height}
+                  />
+                  <Inner onClick={handleInnerClick}>
+                    <h3 src className="subheader accent">
+                      {title}
+                      <img
+                        width="35px"
+                        height="35px"
+                        src="/public/dropdownArrow.png"
+                        alt="Dropdown Icon"
+                      />
+                    </h3>
+
+                    <AdditionalText expanded={isTextExpanded}>
+                      {item.description ? (
+                        <div
+                          dangerouslySetInnerHTML={{
+                            __html: `${item.description}`,
+                          }}
+                        />
+                      ) : null}
+                    </AdditionalText>
+                  </Inner>
+                </Item>
+              );
+            })}
+          </GridThree>
+        </center>
+        <center>
+          <ButtonPrimary href="/contact">talk to us today</ButtonPrimary>
+        </center>
+      </Container>
+    </Section>
+  );
+}
