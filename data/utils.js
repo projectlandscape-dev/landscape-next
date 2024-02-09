5a02df34770eebbb4a3089bbd579b19883ece5f6
loveheenavasu <trivediheenalove@gmail.com>
2024-02-09 16:40:53 +0530

    fix css issue

---
+++ data/utils.js
@@ -0,0 +1,435 @@
+export const tempPages = [
+    {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU1',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM1',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'patio-surfaces',
+        title: 'patio surfaces',
+        uri: '/residential-services/landscape-construction/patio-surfaces/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU2',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM2',
+          slug: 'outdoor-living',
+          uri: '/residential-services/outdoor-living/',
+          title: 'outdoor living'
+        },
+        slug: 'garden-walls',
+        title: 'garden walls',
+        uri: '/residential-services/outdoor-living/garden-walls/'
+      },
+     
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU3',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM3',
+          slug: 'hardscaping',
+          uri: '/residential-services/hardscaping/',
+          title: 'hardscaping'
+        },
+        slug: 'garden-bed-edging',
+        title: 'garden bed edging',
+        uri: '/residential-services/hardscaping/garden-bed-edging/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU4',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM4',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'hardscaping',
+        title: 'hardscaping',
+        uri: '/residential-services/hardscaping/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY5',
+        menuOrder: null,
+        parent: null,
+        slug: 'concrete-curbing',
+        title: 'concrete curbing',
+        uri: '/concrete-curbing/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY6',
+        menuOrder: null,
+        parent: null,
+        slug: 'staircases-stepping-stones',
+        title: 'staircases stepping stones',
+        uri: '/staircases-stepping-stones/'
+      },
+   
+    
+  
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU7',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM7',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape-construction'
+        },
+        slug: 'seating',
+        title: 'seating',
+        uri: '/residential-services/landscape-construction/seating/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU8',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM8',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'concrete-installations',
+        title: 'concrete installations',
+        uri: '/residential-services/concrete-installations/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU9',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM9',
+          slug: 'hardscaping',
+          uri: '/residential-services/hardscaping/',
+          title: 'hardscaping'
+        },
+        slug: 'water-management',
+        title: 'water management',
+        uri: '/residential-services/hardscaping/water-management/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU10',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM10',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'softscaping',
+        title: 'softscaping',
+        uri: '/residential-services/softscaping/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU11',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM11',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'artificial-turf',
+        title: 'artificial turf',
+        uri: '/residential-services/landscape-construction/artificial-turf/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU12',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM12',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'irrigation-landscape-lighting',
+        title: 'irrigation landscape lighting',
+        uri: '/residential-services/irrigation-landscape-lighting/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU13',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM13',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'sod-installation',
+        title: 'sod installation',
+        uri: '/residential-services/landscape-construction/sod-installation/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU14',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM14',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'tree-shrub-planting',
+        title: 'tree shrub planting',
+        uri: '/residential-services/landscape-construction/tree-shrub-planting/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU15',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM15',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'mulches-decorative-rock',
+        title: 'mulches decorative rock',
+        uri: '/residential-services/landscape-construction/mulches-decorative-rock/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY16',
+        menuOrder: null,
+        parent: null,
+        slug: 'gazebos-pergolas',
+        title: 'gazebos pergolas',
+        uri: '/gazebos-pergolas/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY17',
+        menuOrder: null,
+        parent: null,
+        slug: 'landscape-lighting-services-calgary',
+        title: 'landscape lighting services calgary',
+        uri: '/landscape-lighting-services-calgary/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY18',
+        menuOrder: null,
+        parent: null,
+        slug: 'landscape-design',
+        title: 'landscape design',
+        uri: '/landscape-design/'
+      },
+  
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU19',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM19',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'water-features',
+        title: 'water features',
+        uri: '/residential-services/water-features/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU20',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM20',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'fire-pits-fire-places',
+        title: 'fire pits fire places',
+        uri: '/residential-services/landscape-construction/fire-pits-fire-places/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY21',
+        menuOrder: null,
+        parent: null,
+        slug: 'fence-contractor-in-calgary',
+        title: 'fence contractor in calgary',
+        uri: '/fence-contractor-in-calgary/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY22',
+        menuOrder: null,
+        parent: null,
+        slug: 'privacy-screens',
+        title: 'privacy screens',
+        uri: '/privacy-screens/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY23',
+        menuOrder: null,
+        parent: null,
+        slug: 'railings',
+        title: 'railings',
+        uri: '/railings/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU24',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM24',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'outdoor-kitchens',
+        title: 'outdoor kitchens',
+        uri: '/residential-services/landscape-construction/outdoor-kitchens/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY25',
+        menuOrder: null,
+        parent: null,
+        slug: 'portfolio',
+        title: 'portfolio',
+        uri: '/portfolio/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY26',
+        menuOrder: null,
+        parent: null,
+        slug: 'packages',
+        title: 'packages',
+        uri: '/packages/'
+      },
+
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU27',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM27',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'outdoor-living',
+        title: 'outdoor living',
+        uri: '/residential-services/outdoor-living/'
+      },
+  
+  ]
