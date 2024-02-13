d02a2904f8a04cde51b9978f76c5a9cf44e7fe36
loveheenavasu <trivediheenalove@gmail.com>
2024-02-13 13:20:50 +0530

    changes in promo contest

--- data/utils.js
+++ data/utils.js
@@ -431,5 +431,604 @@ export const tempPages = [
         title: 'outdoor living',
         uri: '/residential-services/outdoor-living/'
       },
-  
+
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY28',
+        menuOrder: null,
+        parent: null,
+        slug: 'composite-decking-in-calgary',
+        title: 'composite decking in calgary',
+        uri: '/composite-decking-in-calgary/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY29',
+        menuOrder: null,
+        parent: null,
+        slug: 'pressure-treated-fencing',
+        title: 'pressure treated fencing',
+        uri: '/pressure-treated-fencing/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY30',
+        menuOrder: null,
+        parent: null,
+        slug: 'cedar-fencing',
+        title: 'cedar fencing',
+        uri: '/cedar-fencing/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY31',
+        menuOrder: null,
+        parent: null,
+        slug: 'vinyl-fencing',
+        title: 'vinyl fencing',
+        uri: '/vinyl-fencing/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY32',
+        menuOrder: null,
+        parent: null,
+        slug: 'chain-link-fencing',
+        title: 'chain link fencing',
+        uri: '/chain-link-fencing/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY33',
+        menuOrder: null,
+        parent: null,
+        slug: 'ornamental-fencing',
+        title: 'ornamental fencing',
+        uri: '/ornamental-fencing/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY34',
+        menuOrder: null,
+        parent: null,
+        slug: 'exotic-hardwood-fencing',
+        title: 'exotic hardwood fencing',
+        uri: '/exotic-hardwood-fencing/'
+      },
+
+
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU35',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM35',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'patios',
+        title: 'patios',
+        uri: '/residential-services/landscape-construction/patios/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU36',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM36',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'walkways',
+        title: 'walkways',
+        uri: '/residential-services/landscape-construction/walkways/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU37',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM37',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'driveways',
+        title: 'driveways',
+        uri: '/residential-services/landscape-construction/driveways/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU38',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM38',
+          slug: 'landscape-construction',
+          uri: '/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'permeable-pavers',
+        title: 'permeable pavers',
+        uri: '/landscape-construction/permeable-pavers/'
+      },
+
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU39',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM39',
+          slug: 'landscape-construction',
+          uri: '/residential-services/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'pool-deck',
+        title: 'pool deck',
+        uri: '/residential-services/landscape-construction/pool-deck/'
+      },
+
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU40',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM40',
+          slug: 'landscape-construction',
+          uri: '/landscape-construction/',
+          title: 'landscape construction'
+        },
+        slug: 'deck-and-patio',
+        title: 'deck and patio',
+        uri: '/landscape-construction/deck-and-patio/'
+      },
+
+   
+
+    
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU52',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM52',
+          slug: 'hardscaping',
+          uri: '/residential-services/hardscaping/',
+          title: 'hardscaping'
+        },
+        slug: 'backyard-sports-court-installation',
+        title: 'backyard sports court installation',
+        uri: '/residential-services/hardscaping/backyard-sports-court-installation/'
+      },
+
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY53',
+        menuOrder: null,
+        parent: null,
+        slug: 'commercial-landscaping-calgary',
+        title: 'commercial landscaping calgary',
+        uri: '/commercial-landscaping-calgary/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY54',
+        menuOrder: null,
+        parent: null,
+        slug: 'fence-installation-calgary',
+        title: 'fence installation calgary',
+        uri: '/fence-installation-calgary/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY55',
+        menuOrder: null,
+        parent: null,
+        slug: 'fire-pit',
+        title: 'fire pit',
+        uri: '/fire-pit/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY56',
+        menuOrder: null,
+        parent: null,
+        slug: 'landscape-experts',
+        title: 'landscape experts',
+        uri: '/landscape-experts/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY57',
+        menuOrder: null,
+        parent: null,
+        slug: 'landscaping-calgary',
+        title: 'landscaping calgary',
+        uri: '/landscaping-calgary/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY58',
+        menuOrder: null,
+        parent: null,
+        slug: 'lawn-care-calgary',
+        title: 'lawn care calgary',
+        uri: '/lawn-care-calgary/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY59',
+        menuOrder: null,
+        parent: null,
+        slug: 'lawn-maintenance-alberta',
+        title: 'lawn maintenance alberta',
+        uri: '/lawn-maintenance-alberta/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY60',
+        menuOrder: null,
+        parent: null,
+        slug: 'snow-removal-calgary',
+        title: 'snow removal calgary',
+        uri: '/snow-removal-calgary/'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo3NDY61',
+        menuOrder: null,
+        parent: null,
+        slug: 'sod-installation-calgary',
+        title: 'sod installation calgary',
+        uri: '/sod-installation-calgary/'
+      },
+
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU62',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM62',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'calgary-landscaping-company',
+        title: 'calgary landscaping company',
+        uri: '/residential-services/calgary-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU63',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM63',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'okotoks-landscaping-company',
+        title: 'okotoks landscaping company',
+        uri: '/residential-services/okotoks-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU64',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM64',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'airdrie-landscaping-company',
+        title: 'airdrie landscaping company',
+        uri: '/residential-services/airdrie-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU65',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM65',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'chestermere-landscaping-company',
+        title: 'chestermere landscaping company',
+        uri: 'residential-services/chestermere-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU66',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM66',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'langdon-landscaping-company',
+        title: 'langdon landscaping company',
+        uri: '/residential-services/langdon-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU67',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM67',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'boulder-creek-landscaping-company',
+        title: 'boulder creek landscaping company',
+        uri: '/residential-services/boulder-creek-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU68',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM68',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'heritage-pointe-landscaping-company',
+        title: 'heritage pointe landscaping company',
+        uri: '/residential-services/heritage-pointe-landscaping-company'
+      },
+    
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU69',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM69',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'priddis-landscaping-company',
+        title: 'priddis landscaping company',
+        uri: '/residential-services/priddis-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU70',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM70',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'bearspaw-landscaping-company',
+        title: 'bearspaw landscaping company',
+        uri: '/residential-services/bearspaw-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU71',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM71',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'springbank-landscaping-company',
+        title: 'springbank landscaping company',
+        uri: '/residential-services/springbank-landscaping-company'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU72',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM72',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'commercial-lawn-care',
+        title: 'commercial lawn care',
+        uri: '/residential-services/commercial-lawn-care'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU73',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM73',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'landscape-enhancements',
+        title: 'landscape enhancements',
+        uri: '/residential-services/landscape-enhancements'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU74',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM74',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'commercial-irrigation-and-water-management',
+        title: 'commercial irrigation and water management',
+        uri: '/residential-services/commercial-irrigation-and-water-management'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU75',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM75',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'turf-management',
+        title: 'turf-management',
+        uri: '/residential-services/turf-management'
+      },
+      {
+        __typename: 'Page',
+        content: null,
+        featuredImage: null,
+        children: [],
+        id: 'cG9zdDo2NzU76',
+        menuOrder: null,
+        parent: {
+          __typename: 'Page',
+          id: 'cG9zdDo4NTM76',
+          slug: 'residential-services',
+          uri: '/residential-services/',
+          title: 'residential services'
+        },
+        slug: 'parking-lot-maintenance',
+        title: 'parking lot maintenance',
+        uri: '/residential-services/parking-lot-maintenance'
+      },
   ]
