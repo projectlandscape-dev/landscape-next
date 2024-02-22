625e5ede35dfde5a6c4020fe197a1a9ad74d1baf
loveheenavasu <trivediheenalove@gmail.com>
2024-02-22 18:04:45 +0530

    fix - blocking content

--- components/MainServices/MainServices.js
+++ components/MainServices/MainServices.js
@@ -2,34 +2,39 @@ import Image from "next/image";
 import React from "react";
 import dynamic from "next/dynamic";
 const ButtonUnderline = dynamic(() =>
-  import("../buttons").then((module) => module.ButtonUnderline)
+  import("../buttons").then((module) => module.ButtonUnderline),
+  { ssr: false }
 );
 import styles from "./MainServices.module.scss";
 
 export default function MainServices() {
+
+  const paragraphContent = {
+    title: 'A Calgary Landscape Company you Can Rely On',
+    description: `Welcome to Project Landscape, Calgary's most trusted and extensive
+      residential landscape company, where transforming your outdoor
+      space into a dream oasis is our passion. As Calgary's largest
+      provider of comprehensive landscaping services, we specialize in
+      bringing your vision to life with our expert hardscaping,
+      softscaping, and outdoor living solutions. Our certification in
+      paving stones, decking, and retaining walls guarantees you not
+      only exceptional quality but also innovative design and durable
+      construction. At Project Landscape, we understand that your home
+      is your sanctuary. That's why we're dedicated to enhancing your
+      living experience, ensuring every project reflects your personal
+      style and meets your needs. Choose us for a landscape that
+      elevates your home and lifestyle!`
+  }
+
   return (
     <section>
         <div className="container">
           <div className={styles.text}>
             {/* <div> */}
-              <h2 className="title">
-                A Calgary Landscape Company you Can Rely On
-              </h2>
+              <h2 className="title"> { paragraphContent.title } </h2>
             {/* </div> */}
             <p>
-              Welcome to Project Landscape, Calgary's most trusted and extensive
-              residential landscape company, where transforming your outdoor
-              space into a dream oasis is our passion. As Calgary's largest
-              provider of comprehensive landscaping services, we specialize in
-              bringing your vision to life with our expert hardscaping,
-              softscaping, and outdoor living solutions. Our certification in
-              paving stones, decking, and retaining walls guarantees you not
-              only exceptional quality but also innovative design and durable
-              construction. At Project Landscape, we understand that your home
-              is your sanctuary. That's why we're dedicated to enhancing your
-              living experience, ensuring every project reflects your personal
-              style and meets your needs. Choose us for a landscape that
-              elevates your home and lifestyle!
+              { paragraphContent.description }
             </p>
           </div>
         </div>
