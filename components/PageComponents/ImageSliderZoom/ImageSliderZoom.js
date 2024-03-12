a2578eb07ada0c65ef62348c7fa8f2fb6fff7d59
loveheenavasu <trivediheenalove@gmail.com>
2024-03-12 10:41:43 +0530

    reinstalled node module

--- components/PageComponents/ImageSliderZoom/ImageSliderZoom.js
+++ components/PageComponents/ImageSliderZoom/ImageSliderZoom.js
@@ -1,8 +1,7 @@
+'use client'
 import Image from "next/image";
 import React, { useState, useRef, useEffect } from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { Container, Section } from "../../layoutComponents";
 import styled from "styled-components";
 
