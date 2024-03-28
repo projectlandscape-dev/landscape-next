fae554e598b1193d2f9dca0f8ea659d0a42c64c4
loveheenavasu <trivediheenalove@gmail.com>
2024-03-28 17:23:37 +0530

    added border in texteftwhite section

--- components/PageComponents/TextLeftWhite/TextLeftWhite.js
+++ components/PageComponents/TextLeftWhite/TextLeftWhite.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { Container } from "../../layoutComponents";
+import styled from "styled-components";
 
+const StyledTitle = styled.div`
+  border: 2px solid #093057;
+  background-color: #093057;
+  color : #fff;
+  padding: 10px; 
+  // display: inline-block; 
+`;
 const TextLeftWhite = ({ title, body }) => {
   return (
-      <Container>
-        <center>
-
-        <h1 className="title">{title}</h1>
-        </center>
-        {body ? (
-          <div
-            dangerouslySetInnerHTML={{
-              __html: `${body}`,
-            }}
-          />
-        ) : null}
-      </Container>
+    <Container>
+      <center>
+        <StyledTitle>
+          <h1 className="title">{title}</h1>
+        </StyledTitle>{" "}
+      </center>
+      {body ? (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `${body}`,
+          }}
+        />
+      ) : null}
+    </Container>
   );
 };
 
--- components/testimonials/mainTestimonials.js
+++ components/testimonials/mainTestimonials.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import styled from "styled-components";
 import { ButtonPrimary } from "../buttons";
@@ -62,18 +62,46 @@ const StarRating = React.memo(({ rating }) => {
   );
 });
 
-const ReviewBox = (props) => {
+const ReviewBox = ({ title, review, name }) => {
   return (
     <ReviewWrapper className="spacing">
-      <h3 className="subheader">{props.title}</h3>
-      <p className="italics">"{props.review}"</p>
-      <p className="bold caps">{props.name}</p>
+      <h3 className="subheader">{title}</h3>
+      <p className="italics">"{review}"</p>
+      <p className="bold caps">{name}</p>
       <StarRating />
     </ReviewWrapper>
   );
 };
 
+const testimonials = [
+  {
+    title: "I am certainly happy I made the choice to go with them",
+    review:
+      "After attending the Calgary Home and Garden show with my neighbor and seeing their excellent display we decided to use them for our backyard projects. I am certainly happy I made the choice to go with them. The workers  attention to detail and excellent preparation greatly contributed the success of the project.",
+    name: "Marilyn D.",
+  },
+  {
+    title:
+      "We are thrilled with our patio extension and highly recommend their services",
+    review:
+      "Ryan and Alison along with the entire team at Project Landscape were great to work with. From our initial consultation to the completion of the work every step was communicated clearly and executed well. We are thrilled with our patio extension and highly recommend their services",
+    name: "Tammy B.",
+  },
+  {
+    title: "Project Landscape was an absolute pleasure to work with",
+    review:
+      "They took on a pretty big project in our backyard that included excavating a large portion of the sloped yard, installing a gorgeous retaining wall with a beautiful patio completed in paving stones and decorative rock, a number of privacy screens as well as some shrubs & grass. The  team was superb, demonstrating exceptional skill.",
+    name: "Sabrina",
+  },
+];
 export default function MainTestimonials() {
+
+  const [startIndex, setStartIndex] = useState(0);
+  const itemsPerPage = 1;
+
+  const handleSlideChange = (currentSlide) => {
+    setStartIndex(currentSlide * itemsPerPage);
+  };
   const settings = {
     dots: true,
     infinite: true,
@@ -101,22 +129,15 @@ export default function MainTestimonials() {
         <section>
           <div className="container">
             <SliderWrapper>
-              <Slider {...settings}>
-                <ReviewBox
-                  title="I am certainly happy I made the choice to go with them"
-                  review="After attending the Calgary Home and Garden show with my neighbor and seeing their excellent display we decided to use them for our backyard projects. I am certainly happy I made the choice to go with them. The workers  attention to detail and excellent preparation greatly contributed the success of the project."
-                  name="Marilyn D."
-                />
-                <ReviewBox
-                  title="We are thrilled with our patio extension and highly recommend their services"
-                  review="Ryan and Alison along with the entire team at Project Landscape were great to work with. From our initial consultation to the completion of the work every step was communicated clearly and executed well. We are thrilled with our patio extension and highly recommend their services"
-                  name="Tammy B."
-                />
-                <ReviewBox
-                  title="Project Landscape was an absolute pleasure to work with"
-                  review="They took on a pretty big project in our backyard that included excavating a large portion of the sloped yard, installing a gorgeous retaining wall with a beautiful patio completed in paving stones and decorative rock, a number of privacy screens as well as some shrubs & grass. The  team was superb, demonstrating exceptional skill."
-                  name="Sabrina"
-                />
+              <Slider {...settings} afterChange={handleSlideChange}>
+                {testimonials.map((testimonial, index) => (
+                  <ReviewBox
+                    key={index}
+                    title={testimonial.title}
+                    review={testimonial.review}
+                    name={testimonial.name}
+                  />
+                ))}
               </Slider>
             </SliderWrapper>
           </div>
