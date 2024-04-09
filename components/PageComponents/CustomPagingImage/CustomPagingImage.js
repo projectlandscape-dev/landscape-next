c015307ca6113a72ab4fdccbf5274f5091d65160
projectlandscape-dev <163407454+projectlandscape-dev@users.noreply.github.com>
2024-04-09 06:27:57 -0400

    Custom/paging (#14)
    * fix changes
    
    * added Quick Scroll section
    
    * fix fourrowimage issue
    
    * added custom paging
    
    * fix custom paging section
    
    ---------
    
    Co-authored-by: loveheenavasu <trivediheenalove@gmail.com>

--- components/PageComponents/CustomPagingImage/CustomPagingImage.js
+++ components/PageComponents/CustomPagingImage/CustomPagingImage.js
@@ -1,9 +1,12 @@
-import React from "react";
-import { Container } from "../../layoutComponents";
+import React, { useState } from "react";
 import Image from "next/image";
 import styled from "styled-components";
-import Slider from "react-slick";
-import Link from "next/link";
+import { Button } from "react-bootstrap";
+import styles from "./CustomPagingImage.module.scss";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Section } from "../../layoutComponents";
 
 export const GridAuto = styled.div`
   display: grid;
@@ -11,29 +14,8 @@ export const GridAuto = styled.div`
   grid-gap: 2em;
   margin-top: 2em;
 `;
-const SliderWrapper = styled.div`
-  .slick-prev:before,
-  .slick-next:before {
-    color: var(--clr-accent);
-    font-size: 31px;
-  }
-  .slick-slide {
-    padding: 0px 15px;
-  }
-  .slick-dots {
-    li {
-      height: 30px;
-      width: 30px;
-    }
-  }
-`;
-const ImageContainer = styled.div`
-display : flex;
-justify-content :center;
-align-item : center;
-margin-left: auto;
-margin-right : auto;
-`
+
+
 const CustomPagingImage = ({
   bigImage,
   title,
@@ -42,89 +24,97 @@ const CustomPagingImage = ({
   description,
   descriptionTitle,
 }) => {
-  const settings = {
-    customPaging: function (i) {
-      return (
-        <div>
-          {/* <h3 className="subheader">{colorHeading}</h3> */}
-
-          <div>
-            <Image
-              src={bigImage[i]?.image?.sourceUrl}
-              alt={bigImage[i]?.image?.altText}
-              width={100}
-              height={100}
-            />
-            {/* <h4>{item.title}</h4> */}
-          </div>
-        </div>
-      );
-    },
-    dots: true,
-    dotsClass: "slick-dots slick-thumb",
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+  const [imageIndex, setImageIndex] = useState(2);
+  const handleNextClick = () => {
+    if (imageIndex === bigImage?.length - 1) {
+      setImageIndex(0);
+    } else {
+      setImageIndex(imageIndex + 1);
+    }
+  };
+  const handlePrevClick = () => {
+    if (imageIndex === 0) {
+      setImageIndex(bigImage?.length - 1);
+    } else {
+      setImageIndex(imageIndex - 1);
+    }
   };
   return (
-    <section style={{ marginTop: "4em" }}>
-      <Container>
+    <Section>
+            <h2 className="title mb-5 center">{title}</h2>
+<Container>
+      <div className={styles.imageSlider}>
+        <Button variant="success" className={styles?.prevButton} onClick={handlePrevClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 298 512"
+            fill="currentColor"
+          >
+            <path
+              fillRule="nonzero"
+              d="M285.77 441c16.24 16.17 16.32 42.46.15 58.7-16.16 16.24-42.45 16.32-58.69.16l-215-214.47c-16.24-16.16-16.32-42.45-.15-58.69L227.23 12.08c16.24-16.17 42.53-16.09 58.69.15 16.17 16.24 16.09 42.54-.15 58.7l-185.5 185.04L285.77 441z"
+            />
+          </svg>
+        </Button>
+        <div className="">
+          <Image
+            width={400}
+            height={400}
+            alt={"Image not Found "}
+            src={bigImage?.[imageIndex]?.image?.sourceUrl || ""}
+          />
+          <p className={styles?.imageTitle}>
+            {bigImage?.[imageIndex]?.imageTitle}
+          </p>
+        </div>
+        <Button variant="success" className={styles.nextButton} onClick={handleNextClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 298 512"
+            fill="currentColor"
+          >
+            <path
+              fillRule="nonzero"
+              d="M70.77 499.85c-16.24 16.17-42.53 16.09-58.69-.15-16.17-16.25-16.09-42.54.15-58.7l185.5-185.03L12.23 70.93c-16.24-16.16-16.32-42.45-.15-58.7 16.16-16.24 42.45-16.32 58.69-.15l215.15 214.61c16.17 16.25 16.09 42.54-.15 58.7l-215 214.46z"
+            />
+          </svg>
+        </Button>
+      </div>
+      <div>
         <center>
-          <h1 className="title">{/* {title} */}</h1>
+
+        <h3 className='subheader mt-2'>{colorHeading}</h3>
         </center>
-        <GridAuto>
-          <SliderWrapper>
-            <Slider {...settings}>
-              {bigImage.map((item) => {
-                return (
-                  <ImageContainer key={item} >
-                    <Image
-                      style={{  height: "auto" }}
-                      src={item?.image?.sourceUrl}
-                      alt={item?.image?.altText}
-                      width={700}
-                      height={300}
-                    />
-                    <h3 className="subheader center mt-2">{item.imageTitle}</h3>
-                  </ImageContainer>
-                );
-              })}
-            </Slider>
-          </SliderWrapper>
-        </GridAuto>
-        {/* <center>
-          <h3 className="subheader mt-5">
-            {colorHeading}
-            </h3>
-          <GridAuto>
-            {colorItem.map((item) => {
-              return (
-            <div>
-              <Image
-                key={item}
-                src={item?.image?.sourceUrl}
-                alt={item?.image?.altText}
-                width={100}
-                height={100}
-              />
-              <h4>
-                {item.title}
-              </h4>
-            </div>
-            );
-            })} 
-          </GridAuto>
-        </center> */}
-        <h4 className="subheader mt-5">{descriptionTitle}</h4>
-        <div
-          className="mt-2"
-          dangerouslySetInnerHTML={{
-            __html: `${description}`,
-          }}
-        />
+        <Container>
+          <Row className={styles?.colorDescription}>
+            {bigImage?.map((item, i) => (
+              <Col lg={2}>
+                <div className={styles.descriptionImage} key={i}>
+                  <Image
+                    src={item?.image?.sourceUrl ?? "defaultImage.png"} // Assuming 'defaultImage.png' is your fallback image
+                    width={100}
+                    height={100}
+                    style={{ margin: "auto", borderRadius: "50%" }}
+                    onClick={() => setImageIndex(i)}
+                  />
+                  <p className={styles.imageTitle}>{item?.imageTitle}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+          <h4 className="subheader mt-5">{descriptionTitle}</h4>
+          <div className={styles.descriptionImage}>
+            <div
+              className="mt-2"
+              dangerouslySetInnerHTML={{
+                __html: `${description}`,
+              }}
+            />
+          </div>
+        </Container>
+      </div>
       </Container>
-    </section>
+    </Section>
   );
 };
 
