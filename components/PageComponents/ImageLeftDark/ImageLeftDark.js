895822e94c548f3b8c4532d9ab9031706e375380
loveheenavasu <trivediheenalove@gmail.com>
2024-05-27 10:36:45 +0530

    added alt text in imageleftdark section

--- components/PageComponents/ImageLeftDark/ImageLeftDark.js
+++ components/PageComponents/ImageLeftDark/ImageLeftDark.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Container, Section, FlexMobileOpp } from "components/layoutComponents";
 import { ButtonSecondaryLight } from "components/buttons";
 import Image from "../../Image";
-
+import NextImage from "next/image";
 const Wrapper = styled.div`
   background: var(--clr-accent);
 `;
@@ -63,7 +63,7 @@ export default function ImageLeftDark({
             <Img>
               {imageCopy ? (
                 <LogoWrapper>
-                  <img height="130px" width="130px" src={imageCopy.sourceUrl} />
+                  <NextImage height={130} width={130} src={imageCopy.sourceUrl} alt={imageCopy.altText}/>
                 </LogoWrapper>
               ) : null}
               <Image
@@ -91,6 +91,7 @@ export default function ImageLeftDark({
                     <Image
                       width="160px"
                       src="https://21-pl.purpleparrotwebsites.com/wp-content/uploads/2024/01/image-62-removebg-preview.png"
+                      alt='arrow'
                     />
                   )}
                 </Buttonwrapper>
@@ -103,6 +104,7 @@ export default function ImageLeftDark({
                     <Image
                       width="160px"
                       src="https://21-pl.purpleparrotwebsites.com/wp-content/uploads/2024/01/image-62-removebg-preview.png"
+                      alt='arrow'
                     />
                   )}
                 </Buttonwrapper>
