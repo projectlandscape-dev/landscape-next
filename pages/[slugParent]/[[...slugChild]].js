b84ca16f292eb6c59777fff6d9beb450f1520f5f
loveheenavasu <trivediheenalove@gmail.com>
2024-08-12 14:27:06 +0530

    fixed build issue

--- pages/[slugParent]/[[...slugChild]].js
+++ pages/[slugParent]/[[...slugChild]].js
@@ -38,7 +38,7 @@ export default function Page(props) {
 
   return (
     <LayoutJs>
-    <Seo title={title} description={description} />
+    {/* <Seo title={title} description={description} /> */}
       <div>
         {/* Iterate over ACF flexible fields and pass props to imported component */}
         {hasFlexibleFields && (
