13cdad72a0a093a99ce2cb96a73cf27e6a1d6b70
loveheenavasu <trivediheenalove@gmail.com>
2024-10-18 18:00:16 +0530

    feat: added heading and see more buton

--- components/PageComponents/DropdownMenus/DropdownMenus.js
+++ components/PageComponents/DropdownMenus/DropdownMenus.js
@@ -4,18 +4,41 @@ import {
   AccordionIcon,
   AccordionItem,
   AccordionPanel,
-  Box,
+  Button,
+  Heading,
 } from "@chakra-ui/react";
-import React from "react";
-import { Section, Container } from "../../layoutComponents";
+import React, { useState, useEffect } from "react";
+import { Container } from "../../layoutComponents";
 
 const DropdownMenus = ({ repeaterContent }) => {
-  console.log(repeaterContent, "repeaterContent");
+  const [expandedIndex, setExpandedIndex] = useState(null);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const toggleDescription = (index) => {
+    setExpandedIndex(expandedIndex === index ? null : index);
+  };
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <Container>
       <div className="pb-12 -mt-8">
+        <Heading as={"h5"} className="text-center">
+          Product Information
+        </Heading>
         <Accordion allowToggle>
           {repeaterContent?.map((data, index) => {
+            const shortDescription =
+              data?.heading === "Description"
+                ? data.description.slice(0, 300) + "..."
+                : null;
+
             return (
               <AccordionItem key={index}>
                 <h2>
@@ -24,10 +47,26 @@ const DropdownMenus = ({ repeaterContent }) => {
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
-                <AccordionPanel
-                  className="py-2 px-4"
-                  dangerouslySetInnerHTML={{ __html: `${data.description}` }}
-                />
+                <AccordionPanel className="py-2 px-4">
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        shortDescription && expandedIndex !== index
+                          ? shortDescription
+                          : data.description,
+                    }}
+                  />
+                  {shortDescription && (
+                    <Button
+                      w={"70px"}
+                      h={"30px"}
+                      fontSize={"12px"}
+                      onClick={() => toggleDescription(index)}
+                    >
+                      {expandedIndex === index ? "See Less" : "See More"}
+                    </Button>
+                  )}
+                </AccordionPanel>
               </AccordionItem>
             );
           })}
